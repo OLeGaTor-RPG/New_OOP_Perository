b95fce3c70c3e985d9604f4f4e913ee1dced49cf
OLeGaTor-RPG <62431055+OLeGaTor-RPG@users.noreply.github.com>
2020-09-21 18:44:48 +0300

    Добавлено домашнее задание по ООП

--- Task_1/Task_1/Program.cs
+++ Task_1/Task_1/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace zxcvbnm_
+namespace Task_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Привіт, ДоннУ!");
         }
     }
 }
