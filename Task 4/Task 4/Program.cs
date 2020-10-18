f4ae707504220c7e105164bb7194737399a99299
Oleg-Timchuk <62431055+OLeGaTor-RPG@users.noreply.github.com>
2020-10-18 20:55:18 +0300

    Добавил Масштабирование

--- Task 4/Task 4/Program.cs
+++ Task 4/Task 4/Program.cs
@@ -13,9 +13,10 @@ namespace Task_4
     {
         private int tochkaX { get; set; }
         private int tochkaY { get; set; }
-        public int r;
-        public double area;
-        public double length;
+        private double mash { get; set; }
+        private int r { get; set; }
+        private double area { get; set; }
+        private double length { get; set; }
         public double CalcArea(int a)
         {
             area = r * r * Math.PI;
@@ -48,6 +49,12 @@ namespace Task_4
             Console.WriteLine($"Точка х = {tochkaX}");
             Console.WriteLine($"Точка у = {tochkaY}");
             Console.WriteLine($"Радіус R = {r}");
+            Console.WriteLine($"Масштабованна копія: x = {tochkaX} ; y = {tochkaY} ; R = {mash}");
+        }
+        public double Mashtab(double a)
+        {
+            mash = r / a;
+            return mash;
         }
     }
     class Program
@@ -55,6 +62,7 @@ namespace Task_4
         static void Main()
         {
             int a, b, c;
+            double d;
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.InputEncoding = System.Text.Encoding.Unicode;
             kolo kola = new kolo();
@@ -66,7 +74,10 @@ namespace Task_4
             kola.EnterY(b);
             Console.WriteLine("Введіть радіус R = ");
             c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введіть масштабування 1 до ? :");
+            d = Convert.ToDouble(Console.ReadLine());
             kola.EnterR(c);
+            kola.Mashtab(d);
             kola.GetInfo();
             kola.CalcArea(c);
             kola.CalcLength(c);
