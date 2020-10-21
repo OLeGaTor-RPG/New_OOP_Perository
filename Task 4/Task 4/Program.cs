4a20a13a70d077cc96d269aca35bf160fe8358f7
Oleg-Timchuk <62431055+OLeGaTor-RPG@users.noreply.github.com>
2020-10-21 12:24:27 +0300

    Добавлена обробка помилок

--- Task 4/Task 4/Program.cs
+++ Task 4/Task 4/Program.cs
@@ -6,81 +6,119 @@ using System.Drawing;
 using System.Globalization;
 using System.Linq;
 using System.Text;
-using System.Windows;
+using System.IO;
 namespace Task_4
 {
-    class kolo
+    static class kolo
     {
-        private int tochkaX { get; set; }
-        private int tochkaY { get; set; }
-        private double mash { get; set; }
-        private int r { get; set; }
-        private double area { get; set; }
-        private double length { get; set; }
-        public double CalcArea(int a)
+        private static int tochkaX { get; set; }
+        private static int tochkaY { get; set; }
+        private static double mash { get; set; }
+        private static int r { get; set; }
+        private static double area { get; set; }
+        private static double length { get; set; }
+        public static double CalcArea(int a)
         {
             area = r * r * Math.PI;
             Console.WriteLine($"Площа кола = {area}");
             return area;
         }
-        public double CalcLength(int a)
+        public static double CalcLength(int r)
         {
             length = 2 * r * Math.PI;
             Console.WriteLine($"Довжина кола = {length}");
             return length;
         }
-        public int EnterX(int a)
+        public static int EnterX(int a)
         {
             tochkaX = a;
             return tochkaX;
         }
-        public int EnterR(int a)
+        public static int EnterR(int a)
         {
             r = a;
             return r;
         }
-        public int EnterY(int a)
+        public static int EnterY(int a)
         {
             tochkaY = a;
             return tochkaY;
         }
-        public void GetInfo()
+        public static void GetInfo()
         {
             Console.WriteLine($"Точка х = {tochkaX}");
             Console.WriteLine($"Точка у = {tochkaY}");
             Console.WriteLine($"Радіус R = {r}");
             Console.WriteLine($"Масштабованна копія: x = {tochkaX} ; y = {tochkaY} ; R = {mash}");
         }
-        public double Mashtab(double a)
+        public static double Mashtab(double a)
         {
             mash = r / a;
             return mash;
         }
-    }
-    class Program
-    {
-        static void Main()
+
+        class Program
         {
-            int a, b, c;
-            double d;
-            Console.OutputEncoding = System.Text.Encoding.Unicode;
-            Console.InputEncoding = System.Text.Encoding.Unicode;
-            kolo kola = new kolo();
-            Console.WriteLine("Введіть радіус x = ");
-            a = Convert.ToInt32(Console.ReadLine());
-            kola.EnterX(a);
-            Console.WriteLine("Введіть радіус y = ");
-            b = Convert.ToInt32(Console.ReadLine());
-            kola.EnterY(b);
-            Console.WriteLine("Введіть радіус R = ");
-            c = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введіть масштабування 1 до ? :");
-            d = Convert.ToDouble(Console.ReadLine());
-            kola.EnterR(c);
-            kola.Mashtab(d);
-            kola.GetInfo();
-            kola.CalcArea(c);
-            kola.CalcLength(c);
+            static void Main()
+            {
+                Console.OutputEncoding = System.Text.Encoding.Unicode;
+                Console.InputEncoding = System.Text.Encoding.Unicode;
+                int check;
+                Console.Write("Как Вы хотите принимать елементи масива?(1 - з клавіатури, 2 - з файлу) = ");
+                check = Int32.Parse(Console.ReadLine());
+                if (check == 1)
+                {
+                    int a, b, c;
+                    double d;
+                    Console.WriteLine("Введіть x = ");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    kolo.EnterX(a);
+                    Console.WriteLine("Введіть y = ");
+                    b = Convert.ToInt32(Console.ReadLine());
+                    kolo.EnterY(b);
+                    Console.WriteLine("Введіть радіус R = ");
+                    c = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введіть масштабування 1 до ? :");
+                    d = Convert.ToDouble(Console.ReadLine());
+                    kolo.EnterR(c);
+                    kolo.Mashtab(d);
+                    kolo.GetInfo();
+                    kolo.CalcArea(c);
+                    kolo.CalcLength(c);
+                }
+                else
+                {
+                    int a, b, c;
+                    double d;
+                    List<int> list = new List<int>();
+                    string temp;
+                    FileStream file1 = new FileStream("E:\\x,y,R.txt", FileMode.Open, FileAccess.Read);
+                    StreamReader wr = new StreamReader(file1);
+                    while (true)
+                    {
+                        temp = wr.ReadLine();
+                        if (temp != null)
+                        {
+                            list.Add(Int32.Parse(temp));
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        a = list[0];
+                        b = list[1];
+                        c = list[2];
+                        Console.WriteLine("Введіть масштабування 1 до ? :");
+                        d = Convert.ToDouble(Console.ReadLine());
+                        kolo.EnterR(c);
+                        kolo.Mashtab(d);
+                        kolo.GetInfo();
+                        kolo.CalcArea(c);
+                        kolo.CalcLength(c);
+                    }
+                    wr.Close();
+                }
+            }
         }
     }
 }
--- Task 5/Task 5/Program.cs
+++ Task 5/Task 5/Program.cs
@@ -32,7 +32,9 @@ namespace Task5
         {
             static void Main()
             {
-                Console.OutputEncoding = System.Text.Encoding.Unicode;
+             try
+                {
+                    Console.OutputEncoding = System.Text.Encoding.Unicode;
                 Console.InputEncoding = System.Text.Encoding.Unicode;
                 List<int> list1 = new List<int>();
                 List<int> list2 = new List<int>();
@@ -40,99 +42,105 @@ namespace Task5
                 System.
                 Console.Write("Как Вы хотите принимать елементи масива?(1 - з клавіатури, 2 - з файлу) = ");
                 a = Int32.Parse(Console.ReadLine());
-                if (a == 1)
-                {
-                    bool o = true;
-                    while (o is true)
+
+                    if (a == 1)
                     {
-                        int b;
-                        Console.WriteLine("Введите елемент массива №1 = ");
-                        b = Convert.ToInt32(Console.ReadLine());
-                        list1.Add(b);
-                        Console.WriteLine("Вы закончили?");
-                        string check;
-                        check = Console.ReadLine();
-                        if (check == "да")
+                        bool o = true;
+                        while (o is true)
                         {
-                            foreach (int num in list1)
-                            {              
-                                Console.WriteLine(num);
-                            }
-                            break;
-                        }
-                    }
-                    bool o1 = true;
-                    while(o1 is true)
-                    {
-                        int a1;
-                        Console.WriteLine("Введите елемент массива №2 = ");
-                        a1 = Convert.ToInt32(Console.ReadLine());
-                        list2.Add(a1);
-                        Console.WriteLine("Вы закончили?");
-                        string check2;
-                        check2 = Console.ReadLine();
-                        if (check2 == "да")
-                        {
-                            foreach (int num in list2)
+                            int b;
+                            Console.WriteLine("Введите елемент массива №1 = ");
+                            b = Convert.ToInt32(Console.ReadLine());
+                            list1.Add(b);
+                            Console.WriteLine("Вы закончили?");
+                            string check;
+                            check = Console.ReadLine();
+                            if (check == "да")
                             {
-                                Console.WriteLine(num);
+                                foreach (int num in list1)
+                                {
+                                    Console.WriteLine(num);
+                                }
+                                break;
                             }
-                            break;
+                        }
+                        bool o1 = true;
+                        while (o1 is true)
+                        {
+                            int a1;
+                            Console.WriteLine("Введите елемент массива №2 = ");
+                            a1 = Convert.ToInt32(Console.ReadLine());
+                            list2.Add(a1);
+                            Console.WriteLine("Вы закончили?");
+                            string check2;
+                            check2 = Console.ReadLine();
+                            if (check2 == "да")
+                            {
+                                foreach (int num in list2)
+                                {
+                                    Console.WriteLine(num);
+                                }
+                                break;
+                            }
+                        }
+                        int[] arrA = list1.ToArray();
+                        int[] arrB = list2.ToArray();
+                        int[] arrResult = new int[arrA.Length <= arrB.Length ? arrA.Length : arrB.Length];
+                        arrA = CheckForFive(arrA);
+                        arrResult = NewArray(arrA, arrB, arrResult);
+                        for (int i = 0; i < arrResult.Length; i++)
+                        {
+                            Console.WriteLine(arrResult[i]);
                         }
                     }
-                    int[] arrA = list1.ToArray();
-                    int[] arrB = list2.ToArray();
-                    int[] arrResult = new int[arrA.Length <= arrB.Length ? arrA.Length : arrB.Length];
-                    arrA = CheckForFive(arrA);
-                    arrResult = NewArray(arrA, arrB, arrResult);
-                    for (int i = 0; i < arrResult.Length; i++)
+                    else
                     {
-                        Console.WriteLine(arrResult[i]);
+                        string temp;
+                        FileStream file1 = new FileStream("E:\\1masiv.txt", FileMode.Open, FileAccess.Read);
+                        StreamReader wr = new StreamReader(file1);
+                        while (true)
+                        {
+                            temp = wr.ReadLine();
+                            if (temp != null)
+                            {
+                                list1.Add(Convert.ToInt32(temp));
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        wr.Close();
+
+                        FileStream file2 = new FileStream("E:\\2masiv.txt", FileMode.Open, FileAccess.Read);
+                        StreamReader wr2 = new StreamReader(file2);
+                        while (true)
+                        {
+                            temp = wr2.ReadLine();
+                            if (temp != null)
+                            {
+                                list2.Add(Convert.ToInt32(temp));
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        wr2.Close();
+                        int[] arrA = list1.ToArray();
+                        int[] arrB = list2.ToArray();
+                        int[] arrResult = new int[arrA.Length <= arrB.Length ? arrA.Length : arrB.Length];
+                        arrA = CheckForFive(arrA);
+                        arrResult = NewArray(arrA, arrB, arrResult);
+                        for (int i = 0; i < arrResult.Length; i++)
+                        {
+                            Console.WriteLine(arrResult[i]);
+                        }
                     }
                 }
-                else
+                catch
                 {
-                    string temp;
-                    FileStream file1 = new FileStream("E:\\1masiv.txt", FileMode.Open, FileAccess.Read);
-                    StreamReader wr = new StreamReader(file1);
-                    while (true)
-                    {
-                        temp = wr.ReadLine();
-                        if (temp != null)
-                        {
-                            list1.Add(Convert.ToInt32(temp));
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                    wr.Close();
-
-                    FileStream file2 = new FileStream("E:\\2masiv.txt", FileMode.Open, FileAccess.Read);
-                    StreamReader wr2 = new StreamReader(file2);
-                    while (true)
-                    {
-                        temp = wr2.ReadLine();
-                        if (temp != null)
-                        {
-                            list2.Add(Convert.ToInt32(temp));
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                    wr2.Close();
-                    int[] arrA = list1.ToArray();
-                    int[] arrB = list2.ToArray();
-                    int[] arrResult = new int[arrA.Length <= arrB.Length ? arrA.Length : arrB.Length];
-                    arrA = CheckForFive(arrA);
-                    arrResult = NewArray(arrA, arrB, arrResult);
-                    for(int i = 0; i < arrResult.Length; i++)
-                    {
-                        Console.WriteLine(arrResult[i]);
-                    }
+                    Console.WriteLine("Помилка вводу");
                 }
             }
         }
